9fb0ee7cc37c69dcb6825596f48f9aa180e0a5cb
lDneon <debreuslovemica@gmail.com>
2025-10-02 19:33:07 -0400

    VRsimulator_final
    fixed some bugs, had problems with a prefab that was not compatible with VR.

--- Unity-Project/Assets/EndlessExistence/Common/TextAnimation.cs
+++ Unity-Project/Assets/EndlessExistence/Common/TextAnimation.cs
@@ -1,4 +1,4 @@
-using DG.Tweening;
+
 using TMPro;
 using UnityEngine;
 
@@ -18,23 +18,7 @@ namespace EndlessExistence.Common
             // Set the initial position
             Vector3 initialPosition = initPos;
 
-            // Tween the anchored position using DOTween
-            textMeshPro.rectTransform.DOAnchorPosY(targetPosition.y, moveDuration)
-                .SetEase(Ease.OutQuad)
-                .OnUpdate(() =>
-                {
-                    // Calculate the normalized progress of the move animation
-                    float normalizedProgress = textMeshPro.rectTransform.anchoredPosition.y / targetPosition.y;
-
-                    // Fade out the TextMeshPro text based on the normalized progress
-                    //textMeshPro.DOFade(1f - normalizedProgress, fadeDuration);
-                })
-                .OnComplete(() =>
-                {
-                    //Debug.Log("TextMeshPro move, fade, and move back animation completed");
-                    // Instantly move the TextMeshPro text back to its initial position
-                    InstantMoveText(textMeshPro, initialPosition);
-                });
+            
         }
         
         // Instantly move the TextMeshPro text
